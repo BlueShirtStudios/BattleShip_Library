975ba2e6fdac142781e9844bd2c8e2ea2b26e11d
BlueShirtStudios <daniepotgieter910@gmail.com>
2026-06-28 16:55:36 +0200

    Revised File Structure

---
+++ cls_files/Coordinate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BattleShipCollection
+{
+    public struct Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
