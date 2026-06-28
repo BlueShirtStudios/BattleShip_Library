7f14446a810affd389182e09382397a4e29e776d
BlueShirtStudios <daniepotgieter910@gmail.com>
2026-06-28 16:53:22 +0200

    Built Map Creation, BattleShip Class and Coordinate Classess

---
+++ CoordinateGenerator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace BattleShipCollection
+{
+    public class CoordinateGenerator
+    {
+        public List<int> availableX = new List<int>();
+        public List<int> availableY = new List<int>();
+        private Random randomGene = new Random();
+        private int xFirst;
+        private int xLast;
+        private int yFirst;
+        private int yLast;
+        private int xIndex;
+        private int yIndex;
+
+        public int XFirst
+        {
+            get { return this.xFirst; }
+            set { this.xFirst = value; }
+        }
+        public int XLast
+        {
+            get { return this.xLast; }
+            set { this.xLast = value; }
+        }
+
+        public int YFirst
+        {
+            get { return this.yFirst; }
+            set { this.yFirst = value; }
+        }
+
+        public int YLast
+        {
+            get { return this.yLast; }
+            set { this.yLast = value; }
+        }
+
+        public int XIndex
+        {
+            get { return this.xIndex; }
+            set { this.xIndex = value; }
+        }
+
+        public int YIndex
+        {
+            get { return this.yIndex; }
+            set { this.yIndex = value; }
+        }
+
+        public CoordinateGenerator(int xSize, int ySize)
+        {
+            //List of available values
+            this.availableX = InitializeAllowedXCoords(xSize);
+            this.availableY = InitializeAllowedYCoords(ySize);
+
+            //Last and First Values of the list 
+            this.xLast = availableX[availableX.Count - 1];
+            this.xFirst = availableX[0];
+            this.yLast = availableY[availableY.Count - 1];
+            this.yFirst = availableY[0];
+
+            //Index of an coordinate that is selected
+            this.xIndex = 0;
+            this.yIndex = 0;
+        }
+
+        private List<int> InitializeAllowedXCoords(int xSize)
+        {
+            List<int> xCoords = new List<int>();
+
+            for (int i = 0; i < xSize; i++)
+            {
+                xCoords.Add(i + 1);
+            }
+
+            return xCoords;
+
+        }
+
+        private List<int> InitializeAllowedYCoords(int ySize)
+        {
+            List<int> yCoords = new List<int>();
+
+            for (int i = 0; i < ySize; i++)
+            {
+                yCoords.Add(i + 1);
+            }
+
+            return yCoords;
+
+        }
+
+        public Coordinate GenerateNewCoordinate()
+        {
+            //Get item indexes
+            XIndex = randomGene.Next(XFirst, XLast + 1);
+            YIndex = randomGene.Next(YFirst, YLast + 1);
+
+            //Assign new coords
+            int x = availableX[XIndex - 1];
+            int y = availableY[YIndex - 1];
+
+            //Create new coords struct and return
+            Coordinate newCoord = new Coordinate(x, y);
+            return newCoord;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿using BattleShipCollection;
+using System.Runtime.InteropServices;
+
+class BattleMathConsoleVersion
+{
+    static void Main()
+    {
+        //Code for testing, not final implemetation
+        //Creaet Map
+        Map battleMap = new Map(10, 10);
+
+        //Create Ships
+        BattleShip carrierShip = new BattleShip("Carrier", 1, 5);
+        BattleShip battleShip = new BattleShip("Battleship", 1, 4);
+        BattleShip destroyer = new BattleShip("Destroyer", 1, 3);
+        BattleShip submarine = new BattleShip("Submarine", 1, 3);
+        BattleShip patrolBoat = new BattleShip("Patrol Boat", 1, 2);
+
+        //Add ships to ships to be placed
+        battleMap.AddShip(carrierShip);
+        battleMap.AddShip(battleShip);
+        battleMap.AddShip(destroyer);
+        battleMap.AddShip(submarine);
+        battleMap.AddShip(patrolBoat);
+
+        //Place them on the board
+        battleMap.PlotShips();
+
+        //Build a visual representation of the board
+        CoordStates[,] visualMap = new CoordStates[10, 10];
+        for (int x = 0; x < battleMap.XSize; x++)
+        {
+            for (int y = 0; y < battleMap.YSize; y++)
+            {
+                visualMap[x, y] = battleMap.GetCoordState(battleMap.ConvertIntsToCoord(x, y));
+            }
+        }
+
+        string gridRow;
+        for (int r = 0; r < visualMap.GetLength(0); r++)
+        {
+            gridRow = "";
+            for (int c = 0; c < visualMap.GetLength(1); c++)
+            {
+                gridRow += $"{visualMap[r, c]} ";
+            }
+            Console.WriteLine(gridRow);
+        }
+    }
+}
